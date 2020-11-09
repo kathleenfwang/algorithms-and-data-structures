2bc1ec0a222c85cf389bf0940020dd5e596bd8f6
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-11-08 23:27:40 -0800

    CTCI find min

---
+++ CTCI/stacksnques/find_min.js
@@ -0,0 +1,32 @@
+class Stack {
+    constructor() {
+        // items is (1,2,3,5) have to spread it in the constructor otherwise it will just be the single value
+        this.items = []
+        // have a separate stack to keep track of the mins so it will have a history of the previous min value if the original min gets popped out 
+        this.minStack = [] 
+    }
+    push(item) {
+        // add item to the end 
+        this.items.push(item)
+        // check if new item is smaller than min 
+        if (!this.findMin() || item < this.findMin()) {
+            this.minStack.push(item)
+        }
+    }
+    pop() {
+        // remove last item 
+        this.items.pop()
+        this.minStack.pop() 
+    }
+    findMin() { 
+         if (this.minStack.length === 0) return null 
+         return this.minStack[this.minStack.length - 1]
+    }
+}
+
+let s = new Stack()
+s.push(2)
+s.push(3)
+s.push(5)
+s.push(1)
+console.log(s.findMin())
