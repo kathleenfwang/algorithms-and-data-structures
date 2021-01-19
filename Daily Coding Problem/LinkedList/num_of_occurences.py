0fc96c6e1c4e83c0da1f889ac593fd340c09b6f3
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-01-18 18:28:03 -0800

    add linked list num_of_occurences

---
+++ Daily Coding Problem/LinkedList/num_of_occurences.py
@@ -0,0 +1,20 @@
+"""  Return the no. of times search_for value is there in a linked list.
+  The input list will have at least one element
+  Node is defined as
+
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+  This is method only submission.
+  You only need to complete the method.
+"""
+def count(head, search_for):
+    # Code here
+    count = 0 
+    if not head: return 0 
+    while head:
+        if head.data == search_for:
+            count+=1
+        head = head.next 
+    return count
