d0a252a04bea7cfe5fab76b614e21139b512b8bc
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-01-22 20:28:18 -0800

    repeating nums

---
+++ Daily Coding Problem/Arrays/repeating_nums.py
@@ -0,0 +1,22 @@
+arr1 = [1,3,6,7,10,20]
+arr2 = [3,6,7,20,30]
+result = [3,6,7]
+# we use two pointers to move through searching or matching algos
+# use two pointers to loop through until one array fnishes
+def repeating_nums(arr1,arr2):
+    result = [] 
+    i = 0
+    j = 0
+    while i < len(arr1) and j < len(arr2):
+        # this means arr1 first element is smaller
+        if arr1[i] < arr2[j]:
+            i+=1 
+        elif arr1[i] == arr2[j]:
+            result.append(arr1[i])
+            i+=1
+            j+=1 
+        else: 
+            j+=1 
+    # keep moving until both arrays are finisehd
+    return result 
+print(repeating_nums(arr1,arr2))
