5e6e858f6f1c77c5c550e69f9f987462966af65c
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-02-02 13:09:49 -0800

    all binary nums py

--- Daily Coding Problem/recurssion/all_binary_nums.py
+++ Daily Coding Problem/recurssion/all_binary_nums.py
@@ -9,7 +9,9 @@ def all_binary_nums(nums,index,count):
             # back track and replace changes back to original "?"
             nums[index] = "?"
     else: 
+        # move onto the next index 
         all_binary_nums(nums,index+1,count)
     return count[0]
-nums = "1?01?00?1?"
+nums = "1?1?"  # 1111 1110 1011 1010 
+ # nums = "1?01?00?1?"
 print(all_binary_nums(list(nums),0,[0]))
